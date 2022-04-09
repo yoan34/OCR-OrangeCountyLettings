9f011ddc5ed410ed49cd28f1b72ac21cb0181c3a
yoan <yoan.bousquet@jokosun.com>
2022-04-09 09:30:24 +0200

    refactorisation des app profiles et lettings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lettings.apps.LettingsConfig'
+    'lettings.apps.LettingsConfig',
+    'profiles.apps.ProfilesConfig'
 ]
 
 MIDDLEWARE = [
